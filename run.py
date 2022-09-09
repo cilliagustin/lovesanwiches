c61e67ef69f69f393e7b4e3a3b804b5c416aa60e
Agustin cilli <cilli.agustin@gmail.com>
2022-09-09 13:58:23 +0000

    Fix sintax for heroku

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -27,7 +26,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60")
 
-        data_str = input("Enter your data here: ")   
+        data_str = input("Enter your data here:\n")   
         sales_data = data_str.split(",")
         if validate_data(sales_data):
             print("valid data")
@@ -71,8 +70,7 @@ def calculate_surplus_data(sales_row):
     """
     print("calculating surplus...")
     stock = SHEET.worksheet("stock").get_all_values()
-    stock_row = stock[-1]
-    
+    stock_row = stock[-1]  
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
@@ -126,4 +124,3 @@ def main():
 
 print("Welcome to love sandwiches data automation")
 main()
-
